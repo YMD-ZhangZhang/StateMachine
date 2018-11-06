2df192ec65513a569046e30ce74f0fab01ef4961
zhangzhang <739832729@qq.com>
2018-11-06 19:54:41 +0800

    保存代码文件

--- StateMachine/SmartStateMachineLib.d.ts
+++ StateMachine/SmartStateMachineLib.d.ts
@@ -1,203 +1,148 @@
-declare module SmartStateMachine
-{
-	/**
+declare namespace SmartStateMachine {
+    /**
      * 一个状态/行为
      */
-    class MachineAction
-    {
-        funcOnEnter: Function;// 进入回调
-
-        _name: string;
-        _stateMachine: StateMachine;
-        _transitionList: Array<MachineActionTransition>;// 转换列表
-
+    class MachineAction {
+        funcOnEnter: Function;
+        private _name;
+        private _stateMachine;
+        private _transitionList;
         constructor(name: string, stateMachine: StateMachine);
-		
-        getName();
-        
+        getName(): string;
         /**
          * 添加一个转换
          * @param transition 一个转换
          */
-        addTransition(transition: MachineActionTransition);
-
+        addTransition(transition: MachineActionTransition): void;
         /**
          * 进入状态
          */
-        enter(param: any);
-
+        enter(param: any): void;
         /**
          * 退出状态
          */
-        exit();
-
+        exit(): void;
         /**
          * 触发器
          * @param triggerFlag 触发Flag
          */
-        trigger(triggerFlag: string, param: any = null);
-
+        trigger(triggerFlag: string, param?: any): void;
         /**
          * 设置暂停
          * @param paused 是否暂停
          */
-        setPaused(paused: boolean);
-
-        onDelete();
+        setPaused(paused: boolean): void;
+        onDelete(): void;
     }
-	
-	/**
+    /**
      * 状态机转换
      */
-    class MachineActionTransition
-    {
-        _fromAction: MachineAction;
-        _toAction: MachineAction;
-
-        _paused: boolean = false;
-
-        constructor(fromAction, toAction);
-
+    abstract class MachineActionTransition {
+        private _fromAction;
+        private _toAction;
+        protected _paused: boolean;
+        constructor(fromAction: any, toAction: any);
         /**
          * 激活
          */
-        onEnable();
-
+        abstract onEnable(): any;
         /**
          * 冻结
          */
-        onDisable();
-
+        abstract onDisable(): any;
         /**
          * 触发
          */
-        onTrigger(triggerFlag: string, param: any);
-
+        onTrigger(triggerFlag: string, param: any): void;
         /**
          * 是否暂停
          */
-        setPaused(paused: boolean);
-
-        toNext(param: any);
-
-        onDelete();
+        setPaused(paused: boolean): void;
+        protected toNext(param: any): void;
+        onDelete(): void;
     }
-	
-	/**
+    /**
      * 状态机，与游戏逻辑有关
      */
-    class StateMachine
-    {
-        _nowAction: MachineAction;
-        _actionList: Array<MachineAction> = new Array<MachineAction>();
-
-        createAction(name: string);
-
-        setAction(nowAction: MachineAction);
-
-        getAction();
-
+    class StateMachine {
+        private _nowAction;
+        private _actionList;
+        createAction(name: string): MachineAction;
+        setAction(nowAction: MachineAction): void;
+        getAction(): MachineAction;
         /**
          * 设置暂停
          * @param paused 是否暂停
          */
-        setPaused(paused: boolean);
-
-        trigger(triggerName: string, param: any = null);
-
-        onDelete();
+        setPaused(paused: boolean): void;
+        trigger(triggerName: string, param?: any): void;
+        onDelete(): void;
     }
-	
-	/**
+    /**
      * 转换-延时
      */
-    class TransitionDelay extends MachineActionTransition
-    {
-        _delayTime: number;
-        _nowDelayTime: number;
-
-        _delayLoop: Function;
-        _delayStopFunc: Function;
-        _delayGetDelta: Function;
-        _delayClearFunc: Function;
-
-        constructor(fromAction, toAction, delayLoop: Function, delayStopFunc: Function, delayGetDelta: Function, delayClearFunc: Function);
-
+    class TransitionDelay extends MachineActionTransition {
+        private _delayTime;
+        private _nowDelayTime;
+        private _delayLoop;
+        private _delayStopFunc;
+        private _delayGetDelta;
+        private _delayClearFunc;
+        constructor(fromAction: any, toAction: any, delayLoop: Function, delayStopFunc: Function, delayGetDelta: Function, delayClearFunc: Function);
         /**
          * 自动转换延时
          */
-        setDelayTime(delayTime: number);
-
+        setDelayTime(delayTime: number): void;
         /**
          * Override
          */
-        onEnable();
+        onEnable(): void;
         /**
          * Override
          */
-        onDisable();
-
-        delayUpdate();
-
-        onDelayOver();
-
-        onDelete();
+        onDisable(): void;
+        private delayUpdate();
+        private onDelayOver();
+        onDelete(): void;
     }
-	
-	/**
+    /**
      * 转换-触发器
      */
-    class TransitionTrigger extends MachineActionTransition
-    {
-        _triggerFlag: string;// 触发器Flag
-        _triggerProtectTime: number = 0;// 触发器的保护期(在保护期内触发器无效)
-        _triggerEndTime: number = 0;// 触发器结束时间(结束后触发器无效)
-        _triggerProtecting: boolean;// 触发保护生效中
-
-        _delayBeginFunc: Function;
-        _delayStopFunc: Function;
-        _delayClearFunc: Function;
-
-        constructor(fromAction, toAction, delayBeginFunc: Function, delayStopFunc: Function, delayClearFunc: Function);
-
+    class TransitionTrigger extends MachineActionTransition {
+        private _triggerFlag;
+        private _triggerProtectTime;
+        private _triggerEndTime;
+        private _triggerProtecting;
+        private _delayBeginFunc;
+        private _delayStopFunc;
+        private _delayClearFunc;
+        constructor(fromAction: any, toAction: any, delayBeginFunc: Function, delayStopFunc: Function, delayClearFunc: Function);
         /**
          * 触发器Flag
          */
-        public setTriggerFlag(triggerFlag: string);
-
+        setTriggerFlag(triggerFlag: string): void;
         /**
          * 触发器保护时间
          */
-        public setTriggerProtectTime(time: number);
-
+        setTriggerProtectTime(time: number): TransitionTrigger;
         /**
          * 触发器截止时间
          */
-        public setTriggerEndTime(time: number);
-
+        setTriggerEndTime(time: number): TransitionTrigger;
         /**
          * Override
          */
-        public onEnable();
-
+        onEnable(): void;
         /**
          * Override
          */
-        public onDisable();
-
+        onDisable(): void;
         /**
          * Override
          */
-        public onTrigger(triggerFlag: string, param: any);
-
-        private onTriggerProtectTimeOver()
-        {
-            this._triggerProtecting = false;
-        }
-
+        onTrigger(triggerFlag: string, param: any): void;
+        private onTriggerProtectTimeOver();
         private onTriggerEndTimerOver();
-
-        // Override
-        onDelete();
+        onDelete(): void;
     }
 }
