75e60e0fdde8d5e6b0e977e40d99c74cdb99358f
zhangzhang <739832729@qq.com>
2018-11-08 11:52:22 +0800

    增加tsconfig.json编译文件

--- StateMachine/bin/SmartStateMachineLib.d.ts
+++ StateMachine/bin/SmartStateMachineLib.d.ts
@@ -1,7 +1,4 @@
 declare namespace SmartStateMachine {
-    /**
-     * 一个状态/行为
-     */
     class MachineAction {
         funcOnEnter: Function;
         private _name;
@@ -9,78 +6,41 @@ declare namespace SmartStateMachine {
         private _transitionList;
         constructor(name: string, stateMachine: StateMachine);
         getName(): string;
-        /**
-         * 添加一个转换
-         * @param transition 一个转换
-         */
         addTransition(transition: MachineActionTransition): void;
-        /**
-         * 进入状态
-         */
         enter(param: any): void;
-        /**
-         * 退出状态
-         */
         exit(): void;
-        /**
-         * 触发器
-         * @param triggerFlag 触发Flag
-         */
         trigger(triggerFlag: string, param?: any): void;
-        /**
-         * 设置暂停
-         * @param paused 是否暂停
-         */
         setPaused(paused: boolean): void;
         onDelete(): void;
     }
-    /**
-     * 状态机转换
-     */
+}
+declare namespace SmartStateMachine {
     abstract class MachineActionTransition {
         private _fromAction;
         private _toAction;
         protected _paused: boolean;
         constructor(fromAction: any, toAction: any);
-        /**
-         * 激活
-         */
         abstract onEnable(): any;
-        /**
-         * 冻结
-         */
         abstract onDisable(): any;
-        /**
-         * 触发
-         */
         onTrigger(triggerFlag: string, param: any): void;
-        /**
-         * 是否暂停
-         */
         setPaused(paused: boolean): void;
         protected toNext(param: any): void;
         onDelete(): void;
     }
-    /**
-     * 状态机，与游戏逻辑有关
-     */
+}
+declare namespace SmartStateMachine {
     class StateMachine {
         private _nowAction;
         private _actionList;
         createAction(name: string): MachineAction;
         setAction(nowAction: MachineAction): void;
         getAction(): MachineAction;
-        /**
-         * 设置暂停
-         * @param paused 是否暂停
-         */
         setPaused(paused: boolean): void;
         trigger(triggerName: string, param?: any): void;
         onDelete(): void;
     }
-    /**
-     * 转换-延时
-     */
+}
+declare namespace SmartStateMachine {
     class TransitionDelay extends MachineActionTransition {
         private _delayTime;
         private _nowDelayTime;
@@ -89,25 +49,15 @@ declare namespace SmartStateMachine {
         private _delayGetDelta;
         private _delayClearFunc;
         constructor(fromAction: any, toAction: any, delayLoop: Function, delayStopFunc: Function, delayGetDelta: Function, delayClearFunc: Function);
-        /**
-         * 自动转换延时
-         */
         setDelayTime(delayTime: number): void;
-        /**
-         * Override
-         */
         onEnable(): void;
-        /**
-         * Override
-         */
         onDisable(): void;
         private delayUpdate();
         private onDelayOver();
         onDelete(): void;
     }
-    /**
-     * 转换-触发器
-     */
+}
+declare namespace SmartStateMachine {
     class TransitionTrigger extends MachineActionTransition {
         private _triggerFlag;
         private _triggerProtectTime;
@@ -117,29 +67,11 @@ declare namespace SmartStateMachine {
         private _delayStopFunc;
         private _delayClearFunc;
         constructor(fromAction: any, toAction: any, delayBeginFunc: Function, delayStopFunc: Function, delayClearFunc: Function);
-        /**
-         * 触发器Flag
-         */
         setTriggerFlag(triggerFlag: string): void;
-        /**
-         * 触发器保护时间
-         */
         setTriggerProtectTime(time: number): TransitionTrigger;
-        /**
-         * 触发器截止时间
-         */
         setTriggerEndTime(time: number): TransitionTrigger;
-        /**
-         * Override
-         */
         onEnable(): void;
-        /**
-         * Override
-         */
         onDisable(): void;
-        /**
-         * Override
-         */
         onTrigger(triggerFlag: string, param: any): void;
         private onTriggerProtectTimeOver();
         private onTriggerEndTimerOver();
--- StateMachine/bin/SmartStateMachineLib.js
+++ StateMachine/bin/SmartStateMachineLib.js
@@ -5,9 +5,6 @@ var __extends = (this && this.__extends) || function (d, b) {
 };
 var SmartStateMachine;
 (function (SmartStateMachine) {
-    /**
-     * 一个状态/行为
-     */
     var MachineAction = (function () {
         function MachineAction(name, stateMachine) {
             this._name = name;
@@ -17,42 +14,22 @@ var SmartStateMachine;
         MachineAction.prototype.getName = function () {
             return this._name;
         };
-        /**
-         * 添加一个转换
-         * @param transition 一个转换
-         */
         MachineAction.prototype.addTransition = function (transition) {
             this._transitionList.push(transition);
         };
-        /**
-         * 进入状态
-         */
         MachineAction.prototype.enter = function (param) {
             this._stateMachine.setAction(this);
             if (this.funcOnEnter)
                 this.funcOnEnter(param);
-            // 激活当前Action的所有Transition
             this._transitionList.forEach(function (x) { return x.onEnable(); });
         };
-        /**
-         * 退出状态
-         */
         MachineAction.prototype.exit = function () {
-            // 冻结当前Action的所有Transition
             this._transitionList.forEach(function (x) { return x.onDisable(); });
         };
-        /**
-         * 触发器
-         * @param triggerFlag 触发Flag
-         */
         MachineAction.prototype.trigger = function (triggerFlag, param) {
             if (param === void 0) { param = null; }
             this._transitionList.forEach(function (x) { return x.onTrigger(triggerFlag, param); });
         };
-        /**
-         * 设置暂停
-         * @param paused 是否暂停
-         */
         MachineAction.prototype.setPaused = function (paused) {
             this._transitionList.forEach(function (x) { return x.setPaused(paused); });
         };
@@ -62,22 +39,16 @@ var SmartStateMachine;
         return MachineAction;
     }());
     SmartStateMachine.MachineAction = MachineAction;
-    /**
-     * 状态机转换
-     */
+})(SmartStateMachine || (SmartStateMachine = {}));
+var SmartStateMachine;
+(function (SmartStateMachine) {
     var MachineActionTransition = (function () {
         function MachineActionTransition(fromAction, toAction) {
             this._paused = false;
             this._fromAction = fromAction;
             this._toAction = toAction;
         }
-        /**
-         * 触发
-         */
         MachineActionTransition.prototype.onTrigger = function (triggerFlag, param) { };
-        /**
-         * 是否暂停
-         */
         MachineActionTransition.prototype.setPaused = function (paused) {
             this._paused = paused;
         };
@@ -90,15 +61,15 @@ var SmartStateMachine;
         return MachineActionTransition;
     }());
     SmartStateMachine.MachineActionTransition = MachineActionTransition;
-    /**
-     * 状态机，与游戏逻辑有关
-     */
+})(SmartStateMachine || (SmartStateMachine = {}));
+var SmartStateMachine;
+(function (SmartStateMachine) {
     var StateMachine = (function () {
         function StateMachine() {
             this._actionList = new Array();
         }
         StateMachine.prototype.createAction = function (name) {
-            var action = new MachineAction(name, this);
+            var action = new SmartStateMachine.MachineAction(name, this);
             this._actionList.push(action);
             return action;
         };
@@ -108,10 +79,6 @@ var SmartStateMachine;
         StateMachine.prototype.getAction = function () {
             return this._nowAction;
         };
-        /**
-         * 设置暂停
-         * @param paused 是否暂停
-         */
         StateMachine.prototype.setPaused = function (paused) {
             this._nowAction.setPaused(paused);
         };
@@ -125,9 +92,9 @@ var SmartStateMachine;
         return StateMachine;
     }());
     SmartStateMachine.StateMachine = StateMachine;
-    /**
-     * 转换-延时
-     */
+})(SmartStateMachine || (SmartStateMachine = {}));
+var SmartStateMachine;
+(function (SmartStateMachine) {
     var TransitionDelay = (function (_super) {
         __extends(TransitionDelay, _super);
         function TransitionDelay(fromAction, toAction, delayLoop, delayStopFunc, delayGetDelta, delayClearFunc) {
@@ -137,22 +104,13 @@ var SmartStateMachine;
             this._delayGetDelta = delayGetDelta;
             this._delayClearFunc = delayClearFunc;
         }
-        /**
-         * 自动转换延时
-         */
         TransitionDelay.prototype.setDelayTime = function (delayTime) {
             this._delayTime = delayTime;
         };
-        /**
-         * Override
-         */
         TransitionDelay.prototype.onEnable = function () {
             this._nowDelayTime = 0;
             this._delayLoop(1, this, this.delayUpdate);
         };
-        /**
-         * Override
-         */
         TransitionDelay.prototype.onDisable = function () {
             this._delayStopFunc(this, this.delayUpdate);
         };
@@ -171,44 +129,32 @@ var SmartStateMachine;
             this._delayClearFunc(this);
         };
         return TransitionDelay;
-    }(MachineActionTransition));
+    }(SmartStateMachine.MachineActionTransition));
     SmartStateMachine.TransitionDelay = TransitionDelay;
-    /**
-     * 转换-触发器
-     */
+})(SmartStateMachine || (SmartStateMachine = {}));
+var SmartStateMachine;
+(function (SmartStateMachine) {
     var TransitionTrigger = (function (_super) {
         __extends(TransitionTrigger, _super);
         function TransitionTrigger(fromAction, toAction, delayBeginFunc, delayStopFunc, delayClearFunc) {
             _super.call(this, fromAction, toAction);
-            this._triggerProtectTime = 0; // 触发器的保护期(在保护期内触发器无效)
-            this._triggerEndTime = 0; // 触发器结束时间(结束后触发器无效)
+            this._triggerProtectTime = 0;
+            this._triggerEndTime = 0;
             this._delayBeginFunc = delayBeginFunc;
             this._delayStopFunc = delayStopFunc;
             this._delayClearFunc = delayClearFunc;
         }
-        /**
-         * 触发器Flag
-         */
         TransitionTrigger.prototype.setTriggerFlag = function (triggerFlag) {
             this._triggerFlag = triggerFlag;
         };
-        /**
-         * 触发器保护时间
-         */
         TransitionTrigger.prototype.setTriggerProtectTime = function (time) {
             this._triggerProtectTime = time;
             return this;
         };
-        /**
-         * 触发器截止时间
-         */
         TransitionTrigger.prototype.setTriggerEndTime = function (time) {
             this._triggerEndTime = time;
             return this;
         };
-        /**
-         * Override
-         */
         TransitionTrigger.prototype.onEnable = function () {
             if (this._triggerProtectTime > 0) {
                 this._triggerProtecting = true;
@@ -221,15 +167,9 @@ var SmartStateMachine;
                 this._triggerProtecting = false;
             }
         };
-        /**
-         * Override
-         */
         TransitionTrigger.prototype.onDisable = function () {
             this._delayStopFunc(this, this.onTriggerProtectTimeOver);
         };
-        /**
-         * Override
-         */
         TransitionTrigger.prototype.onTrigger = function (triggerFlag, param) {
             if (this._triggerFlag == triggerFlag && !this._triggerProtecting) {
                 this.toNext(param);
@@ -241,11 +181,10 @@ var SmartStateMachine;
         TransitionTrigger.prototype.onTriggerEndTimerOver = function () {
             this._triggerProtecting = true;
         };
-        // Override
         TransitionTrigger.prototype.onDelete = function () {
             this._delayClearFunc(this);
         };
         return TransitionTrigger;
-    }(MachineActionTransition));
+    }(SmartStateMachine.MachineActionTransition));
     SmartStateMachine.TransitionTrigger = TransitionTrigger;
 })(SmartStateMachine || (SmartStateMachine = {}));
